5179afa3bbd520ffb2abd9924e222cf1ba8f7be0
Burning_TNT <pangyl08@163.com>
2025-12-21 13:52:28 +0800

    Feature: Integrate international EasyTier public servers. (#110)

--- src/easytier/publics.rs
+++ src/easytier/publics.rs
@@ -3,8 +3,10 @@ use crate::controller::Room;
 pub type PublicServers = Vec<String>;
 
 pub fn fetch_public_nodes(_: &Room) -> PublicServers {
-    vec![
-        "tcp://public.easytier.top:11010".into(),
-        "tcp://public2.easytier.cn:54321".into()
-    ]
+    Vec::from([
+        "tcp://public.easytier.top:11010",
+        "tcp://public2.easytier.cn:54321",
+        "https://etnode.zkitefly.eu.org/node1",
+        "https://etnode.zkitefly.eu.org/node2",
+    ].map(|s| s.into()))
 }
