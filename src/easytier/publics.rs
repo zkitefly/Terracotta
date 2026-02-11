8bd718e408943338decbc116b51b5002dee66af2
Zkitefly <64117916+zkitefly@users.noreply.github.com>
2026-02-11 11:53:15 +0800

    Update publics.rs

--- src/easytier/publics.rs
+++ src/easytier/publics.rs
@@ -6,6 +6,8 @@ pub fn fetch_public_nodes(_: &Room, mut external_nodes: PublicServers) -> Public
     external_nodes.extend_from_slice(&[
         "tcp://public.easytier.top:11010",
         "tcp://public2.easytier.cn:54321",
+        "https://etnode.zkitefly.eu.org/-node1",
+        "https://etnode.zkitefly.eu.org/-node2",
         "https://etnode.zkitefly.eu.org/node1",
         "https://etnode.zkitefly.eu.org/node2",
     ].map(|s| s.into()));
